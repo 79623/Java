1ef1bfd4b0d38c9a187d60e579839b13a8342ad7
Snehal patil_KH <145040477+79623@users.noreply.github.com>
2024-03-16 22:27:57 +0530

    Add files via upload

---
+++ Add1.java
@@ -0,0 +1,30 @@
+package in.cdac.kharghar;
+
+public class Add1 {
+	public static int sum(int  n1 , int n2) {
+//		System.out.println("sum is :"+(n1+n2));
+		int result= n1+n2;
+		return result;
+		
+	}
+	//public void csum(int n1,) {
+		
+		
+	//}
+	
+	
+
+	public static void main(String[] args) {
+		//int n1=10;
+		//int n2=20;
+		int n1=10;
+		int n2=20;
+		
+		int sum1=Add1.sum(n1, n2);
+		System.out.println(sum1);
+		
+		
+	}
+
+}
+
