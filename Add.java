130e87464c3139794214563accdfd59037fb151e
Snehal patil_KH <145040477+79623@users.noreply.github.com>
2024-03-16 22:28:16 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,27 @@
+package in.cdac.kharghar;
+
+public class Add {
+	public static void sum(String  n1 , String n2) {
+		System.out.println("sum is :"+(n1+n2));
+		
+	}
+	//public void csum(int n1,) {
+		
+		
+	//}
+	
+	
+
+	public static void main(String[] args) {
+		//int n1=10;
+		//int n2=20;
+		String n1="snehal";
+		String n2="patilr";
+		
+		Vahicle.sum(n1, n2);
+		
+		
+	}
+
+}
+}
